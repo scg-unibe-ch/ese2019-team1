70f39cf380504555a0079a76f4f00c983b5c3123
Adrian Schmucker <adrian.schmucker@students.unibe.ch>
2019-10-09 11:44:04 +0200

    implemented register/login/logout functions

--- Ionic4FirebaseUserNameLogin/src/app/service/auth.service.ts
+++ Ionic4FirebaseUserNameLogin/src/app/service/auth.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import * as firebase from 'firebase/app';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
 
-  constructor() { }
+    constructor() {
+    }
+
+    //functions to register user
+    registerUser(value) {
+        return new Promise<any>((resolve, reject) => {
+            firebase.auth().createUserWithEmailAndPassword(value.email, value.password).then(
+                res => resolve(res),
+                err => reject(err))
+        })
+    }
+
+    //login user, works with email/pass. maybe change to username/pass but then we need
+    // to adapt the firebase api
+    loginUser(value) {
+        return new Promise<any>((resolve, reject) => {
+            firebase.auth().signInWithEmailAndPassword(value.email, value.pass).then(
+                res => resolve(res),
+                err => reject(err))
+        })
+    }
+
+    logoutUser(value) {
+        return new Promise<any>((resolve, reject) => {
+                return new Promise<any>((resolve, reject) => {
+                    if (firebase.auth().currentUser) {
+                        firebase.auth().signOut().then(() => {
+                            console.log("Log out");
+                            resolve();
+                        }).catch((error) => {
+                            reject();
+                        });
+                    }
+                })
+            }
+        )
+    }
+
+    userDetails() {
+        return firebase.auth().currentUser
+    }
 }
