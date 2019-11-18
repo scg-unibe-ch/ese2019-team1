a885b5406dce40be8a20ff0d26dee9be01199454
Adrian <adrian.schmucker@students.unibe.ch>
2019-11-18 16:53:27 +0100

    bug fix: fixed environment variables for firestorage

--- EMA/src/environments/environment.ts
+++ EMA/src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
     authDomain: 'eseproject2019team1.firebaseapp.com',
     databaseURL: 'https://eseproject2019team1.firebaseio.com',
     projectId: 'eseproject2019team1',
-    storageBucket: '',
+    storageBucket: 'eseproject2019team1.appspot.com',
     messagingSenderId: '864255872685',
     appId: '1:864255872685:web:278a10ea559d9c1dd1c024',
     measurementId: 'G-TLEMT3JTVJ'
