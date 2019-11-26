f01330d69cb4d8ffc2b0fd96485523f5c71a5441
Joel Hari <joel.hari@students.unibe.ch>
2019-11-26 16:19:04 +0100

    Improved Feed Page

--- EMA/src/app/components/event-view/event-view.component.ts
+++ EMA/src/app/components/event-view/event-view.component.ts
@@ -1,47 +1,44 @@
-import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, HostListener, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-event-view',
   templateUrl: './event-view.component.html',
   styleUrls: ['./event-view.component.scss'],
 })
-export class EventViewComponent implements OnInit {
+export class EventViewComponent implements OnInit, AfterViewInit {
 
   private events = new Array();
-  private eventsShown = new Array();
 
   private width;
-  private height;
   private aspectRatio;
-
-  private columns = new Array();
-
-  private services = new Array();
 
   @Input() self: EventViewComponent;
 
   constructor() {
     this.events = [
       {title: 'title_1', text: 'this is a text that needs to be shown on the card. It describes the event.',
-        image: '/assets/images/im5_lowres.jpg', service: 'photo'},
+        image: '/assets/images/im5_lowres.jpg', service: 'photo', show: true},
       {title: 'title_2', text: 'this is a text that needs to be shown on the card. It describes the event.',
-        image: '/assets/images/im7_lowres.jpg', service: 'other'},
+        image: '/assets/images/im7_lowres.jpg', service: 'other', show: true},
       {title: 'title_3', text: 'this is a text that needs to be shown on the card. It describes the event.',
-        image: '/assets/images/im7_lowres.jpg', service: 'music'},
+        image: '/assets/images/im7_lowres.jpg', service: 'music', show: true},
       {title: 'title_4', text: 'this is a text that needs to be shown on the card. It describes the event.',
-        image: '/assets/images/im7_lowres.jpg', service: 'photo'},
+        image: '/assets/images/im7_lowres.jpg', service: 'photo', show: true},
       {title: 'title_5', text: 'this is a text that needs to be shown on the card. It describes the event.',
-        image: '/assets/images/im7_lowres.jpg', service: 'music'},
+        image: '/assets/images/im7_lowres.jpg', service: 'music', show: true},
       {title: 'title_6', text: 'this is a text that needs to be shown on the card. It describes the event.',
-        image: '/assets/images/im7_lowres.jpg', service: 'venue'},
+        image: '/assets/images/im7_lowres.jpg', service: 'venue', show: true},
       {title: 'title_7', text: 'this is a text that needs to be shown on the card. It describes the event.',
-        image: '/assets/images/im7_lowres.jpg', service: 'venue'}
+        image: '/assets/images/im7_lowres.jpg', service: 'venue', show: true}
     ];
-    this.reFit();
-    this.selectService('');
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.reFit();
+    this.selectService('');
   }
 
   @HostListener('window:resize', ['$event'])
@@ -50,31 +47,33 @@ export class EventViewComponent implements OnInit {
   }
 
   reFit() {
-    this.width = self.innerWidth;
-    this.height = self.innerHeight;
-    this.aspectRatio = this.width / this.height;
+    const screenWidth = self.innerWidth;
+    const screenHeight = self.innerHeight;
+    this.aspectRatio = screenWidth / screenHeight;
     const events = document.getElementsByClassName('event');
     let i = 0;
-    const eventWidth = 100 / (Math.floor(this.aspectRatio + 1));
+    this.width = 100 / (Math.floor(this.aspectRatio + 1));
     for (i = 0; i < events.length; i++) {
-      events[i].setAttribute('style', 'width: ' + eventWidth + '%;');
+      events[i].setAttribute('style', 'width: ' + this.width + '%;');
     }
   }
 
   public selectService(services) {
-    this.eventsShown = new Array();
     if (services === '') {
       let i = 0;
       for (i = 0; i < this.events.length; i++) {
-        this.eventsShown.push(this.events[i]);
+        this.events[i].show = true;
       }
     } else {
+      let i = 0;
+      for (i = 0; i < this.events.length; i++) {
+        this.events[i].show = false;
+      }
       let e = 0;
       for (e = 0; e < services.length; e++) {
-        let i = 0;
         for (i = 0; i < this.events.length; i++) {
           if (this.events[i].service === services[e]) {
-            this.eventsShown.push(this.events[i]);
+            this.events[i].show = true;
           }
         }
       }
