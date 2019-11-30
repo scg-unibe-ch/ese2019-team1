ac95875a11e90a50b46019a4e739c96978a4ea4c
Adrian <adrian.schmucker@students.unibe.ch>
2019-11-30 17:55:27 +0100

    deleted out folder (pushed by accident)

--- EMA/out-tsc/app/app/services/profile.js
+++
@@ -1,9 +0,0 @@
-export var Categories;
-(function (Categories) {
-    Categories[Categories["venue"] = 0] = "venue";
-    Categories[Categories["music"] = 1] = "music";
-    Categories[Categories["photo"] = 2] = "photo";
-    Categories[Categories["catering"] = 3] = "catering";
-    Categories[Categories["other"] = 4] = "other";
-})(Categories || (Categories = {}));
-//# sourceMappingURL=profile.js.map
