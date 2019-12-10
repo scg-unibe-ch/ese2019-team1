ab8cdf8b5819675b975bea744af4d060cd61b848
Adrian <adrian.schmucker@students.unibe.ch>
2019-12-10 16:24:20 +0100

    reset password feature on login page

--- EMA/out-tsc/app/app/services/img.js
+++
@@ -1,8 +0,0 @@
-export class Img {
-    constructor(img) {
-        this.createdAt = new Date();
-        this.img = img;
-        this.name = img.name;
-    }
-}
-//# sourceMappingURL=img.js.map
--- EMA/src/app/pages/signup/signup.page.ts
+++ EMA/src/app/pages/signup/signup.page.ts
@@ -80,4 +80,6 @@ export class SignupPage implements OnInit {
                 }
             );
     }
+
+
 }
